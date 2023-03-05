2abdd9eb383631ab628ca4122e8bc9274a2bc1fd
Nikodem Zbaraza <nikozet@interia.pl>
2023-03-05 17:02:57 +0100

    initial commit

---
+++ src/main/java/com/example/runrate/RunRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.runrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/runrate/RunRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
