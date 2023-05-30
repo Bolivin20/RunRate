350115cdefa947823d23fbdc388e2c77c36028b1
Nikodem Zbaraza <nikozet@interia.pl>
2023-05-30 15:34:08 +0200

    added levels integration and display

--- frontend/runrate-frontend/src/components/UserDashboard/UserDashboard.js
+++ frontend/runrate-frontend/src/components/UserDashboard/UserDashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../resources/img/logo.svg";
 import "./UserDashboard.css";
 
@@ -7,25 +7,89 @@ const handleLogout = () => {
 };
 
 const UserDashboard = () => {
-    return (
-        <div className="Profile-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="Profile-dashboard">
-            <h2>User</h2>
-            <h2>20lvl</h2>
-            <progress max="100" value="80" title="80%"></progress>
-        <ol>
-      <li><a>Options</a>
-        <ul>
-          <li><a href="/add-activity">Add new run</a></li>
-          <li><a href="/activities">Show activities</a></li>
-          <li><a href="/" onClick={handleLogout}>Logout</a></li>
-        </ul>
-      </li>
-        </ol>
-            </div>
-        </div>
-    );
+  const [points, setPoints] = useState(0);
+  const [level, setLevel] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return;
     }
+    fetch("http://localhost:8080/user/profile/points", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPoints(data.points);
+        setLevel(Math.floor(points / 10) + 1);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    fetch("http://localhost:8080/user/profile/name", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setName(data.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [points]);
+
+  const calculateProgress = () => {
+    return points % 10;
+  };
+
+  return (
+    <div className="Profile-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="Profile-dashboard">
+        <h2>{name}</h2>
+        <h2>{level}lvl</h2>
+        <progress
+          max="10"
+          value={calculateProgress()}
+          title={`${calculateProgress()}%`}
+        ></progress>
+        <ol>
+          <li>
+            <a>Options</a>
+            <ul>
+              <li>
+                <a href="/add-activity">Add new run</a>
+              </li>
+              <li>
+                <a href="/activities">Show activities</a>
+              </li>
+              <li>
+                <a href="/" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
 
 export default UserDashboard;
--- frontend/runrate-frontend/src/views/Profile/Profile.js
+++ frontend/runrate-frontend/src/views/Profile/Profile.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 function Profile() {
   const [trainings, setTrainings] = useState([]);
+  const [points, setPoints] = useState(0);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
@@ -28,40 +29,69 @@ function Profile() {
         return response.json();
       })
       .then((data) => {
-        //console.log(data && data.trainings);
-        
-          setTrainings(data);
-          console.log(data);
-                
+        setTrainings(data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+
+    fetch('http://localhost:8080/user/profile/points', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPoints(data.points);
+        console.log(data.points);
       })
       .catch((error) => {
         setError(error);
       });
   }, []);
 
+  const calculateProgress = () => {
+    return (points / 1000) * 100; // Assuming maximum points is 10,000
+  };
+
+  const lastFourTrainings = trainings.slice(-4); // Get the last four trainings
+
   return (
     <div className="App">
       <div className="Profile-container">
         <UserDashboard />
         <div className="Profile-axis">
-          <progress max="100" value="80" title="80%">
-            80%
+          <progress max="100" value={calculateProgress()} title={`${calculateProgress()}%`}>
+            {calculateProgress()}%
           </progress>
           <h1>Your progress in preparing for the marathon.</h1>
+          
         </div>
-
+        {points > 1000 && <p1>You are ready!</p1>}
         <div className="Profile-activities">
           <h1>Last activities:</h1>
           <div className="Profile-activity">
-            {trainings &&
-              trainings.length > 0 &&
-              trainings.map((training) => (
-                <Activity key={training.id} id={training.id} trainingDate={training.trainingDate} hours={training.hours} minutes={training.minutes} distance={training.distance} elevation={training.elevation} trainingPoints={training.trainingPoints} />
+            {lastFourTrainings &&
+              lastFourTrainings.length > 0 &&
+              lastFourTrainings.map((training) => (
+                <Activity
+                  key={training.id}
+                  id={training.id}
+                  trainingDate={training.trainingDate}
+                  hours={training.hours}
+                  minutes={training.minutes}
+                  distance={training.distance}
+                  elevation={training.elevation}
+                  trainingPoints={training.trainingPoints}
+                />
               ))}
           </div>
-          <div className="Error-message">
-            {error && <p>{error.message}</p>}
-          </div>
+          <div className="Error-message">{error && <p>{error.message}</p>}</div>
         </div>
         <div className="Profile-buttons">
           <Link to="/add-activity">
@@ -76,4 +106,4 @@ function Profile() {
   );
 }
 
-export default Profile
+export default Profile;
